675bddb953a96a84b108c3a65a73ae36cbcfbef6
Magnusmajo <alexisoblivion@gmail.com>
2024-07-25 23:01:32 -0300

    Pricing

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,19 @@
 export default class Building {
   constructor(sqft) {
-      this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    // Setting the _sqft property to the value passed in the constructor
+    this._sqft = sqft;
   }
 
-  // Getter for the 'sqft' attribute
+  // Setter for the sqft property
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
+  // Getter for the sqft property
   get sqft() {
-      return this._sqft;
-  }
-
-  // Abstract method: subclasses must override this
-  evacuationWarningMessage() {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
+    return this._sqft;
   }
 }
