defdb1496f0785e0efe8cbd1c1c69901c25dbb9d
Magnusmajo <alexisoblivion@gmail.com>
2024-12-09 11:10:33 -0300

    More

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -5,6 +5,7 @@ from typing import Any, Mapping, TypeVar, Union
 
 T = TypeVar('T')
 
+
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     """
     Retrieve a value from a dictionary using a given key, returning a
