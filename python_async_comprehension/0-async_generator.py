9b7bb77fd7a02e70e5a4c8a812a3ee0eb6a7d69a
Magnusmajo <alexisoblivion@gmail.com>
2024-12-10 09:37:08 -0300

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,11 +2,11 @@
 """Module Generator Async"""
 
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """It generates aleatory numbers"""
     for _ in range(10):
         await asyncio.sleep(1)
