eedd2464139c791bff47dba3804af0f4f7f1a33c
Magnusmajo <alexisoblivion@gmail.com>
2024-12-12 12:53:04 -0300

    Task

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -11,6 +11,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(
         n: int, max_delay: int
         ) -> List[float]:
-    """This function waits for n tasks to complete and returns a list of their completion times in ascending order."""
+    """This function waits for n tasks to
+    complete and returns a list of their
+    completion times in ascending order."""
     task = [task_wait_random(max_delay)for _ in range(n)]
     return [await i for i in asyncio.as_completed(task)]
