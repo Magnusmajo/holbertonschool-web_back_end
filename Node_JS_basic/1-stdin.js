806e66d67784e0b2d121d2d823db3b50b12ca569
Magnusmajo <alexisoblivion@gmail.com>
2025-01-26 16:44:43 -0300

    Executing basic javascript with Node JS

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,7 @@
-console.log('Welcome to Holberton School, what is your name?');
+const displayMessage = require('./0-console');
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
+displayMessage('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is: ${name}`);
-  if (process.stdin.isTTY) {
-    process.exit();
-  } else {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
-});
+process.stdin.on('data', (data) => process.stdout.write(`Your name is: ${data}`));
+
+process.stdin.on('end', () => displayMessage('This important software is now closing'));
