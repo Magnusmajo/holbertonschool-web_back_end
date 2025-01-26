5a886633dd5dc68ccaee897a92356a7abae70a91
Magnusmajo <alexisoblivion@gmail.com>
2025-01-26 17:02:05 -0300

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 1245;
 
 app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
+});
+
+app.get('/students', (_, res) => {
+  const filePath = path.join(__dirname, 'database.csv');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading the database file');
+      return;
+    }
+    const lines = data.split('\n');
+    const students = lines.slice(1).map(line => line.split(',')[0]);
+    res.status(200).send(`Number of students: ${students.length}\nList of students: ${students.join(', ')}`);
+  });
 });
 
 app.listen(port, () => {
