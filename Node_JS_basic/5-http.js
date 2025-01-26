7fd736bf539e6b83f790e2d107b69933625f313e
Magnusmajo <alexisoblivion@gmail.com>
2025-01-26 17:06:11 -0300

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,45 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
 
-const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    const database = process.argv[2];
-    if (!database) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end('Database file not provided');
-      return;
-    }
+const router = Router();
 
-    fs.readFile(database, 'utf8', (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Cannot load the database');
-        return;
-      }
+router.get('/', AppController.getHomepage);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
-      const lines = data.split('\n').filter(line => line.trim() !== '');
-      const students = lines.slice(1).map(line => line.split(','));
-      const csStudents = students.filter(student => student[3] === 'CS');
-      const sweStudents = students.filter(student => student[3] === 'SWE');
-
-      let responseText = 'This is the list of our students\n';
-      responseText += `Number of students: ${students.length}\n`;
-      responseText += `Number of students in CS: ${csStudents.length}. List: ${csStudents.map(student => student[0]).join(', ')}\n`;
-      responseText += `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map(student => student[0]).join(', ')}`;
-
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(responseText);
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
-});
-
-app.listen(1245);
-
-module.exports = app;
+export default router;
